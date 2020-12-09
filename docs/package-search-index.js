620f334cce19ffc00ba3bea4219ffaae98d2c965
banalna <judas.priest999@gmail.com>
2020-12-09 15:39:32 +0300

    generate docs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.pipservices3.rpc.build"},{"l":"org.pipservices3.rpc.clients"},{"l":"org.pipservices3.rpc.connect"},{"l":"org.pipservices3.rpc.services"}];updateSearchResults();
