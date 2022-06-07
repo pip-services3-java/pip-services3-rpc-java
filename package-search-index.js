4b628953513f2e8d9e55882d8dbf8a5ce6e3d35b
Danil Prisiazhnyi <judas.priest999@gmail.com>
2022-06-07 18:47:27 +0400

    clean files

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.pipservices3.rpc.build"},{"l":"org.pipservices3.rpc.clients"},{"l":"org.pipservices3.rpc.connect"},{"l":"org.pipservices3.rpc.services"},{"l":"org.pipservices3.rpc.test"}];updateSearchResults();
